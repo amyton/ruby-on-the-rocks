0d1478ee776b7d01ea209603d4db5d928cde1d31
Amy Ton <amyhton@gmail.com>
2013-09-28 14:23:22 -0500

    Add serve method, and new instance of Cocktail class

--- blueberry-lavender-mojito.rb
+++ blueberry-lavender-mojito.rb
@@ -13,4 +13,16 @@ class Cocktail
     @ingredients << ingredients
   end
 
+  def serve
+    "Done! Enjoy your #{name}!"
+  end
 end
+
+blm = Cocktail.new("blueberry lavender mojito")
+puts blm.ingredients("one bunch mint leaves", "1/2 cup blueberries", "juice of 1/2 lime", "1 1/2 white rum Club soda", "2 cups granulated sugar", "2 tbs ried lavender blossoms", "2 cups water")
+
+puts blm.steps # => []
+puts blm.steps[0] = "Make lavender simple syrup: Bring 2 cups water, 2 cups granulated sugar, 2 tablespoons dired lavender blossoms to a boil in a saucepan, stirring so that all the sugar has dissolved."
+
+
+puts blm.serve
